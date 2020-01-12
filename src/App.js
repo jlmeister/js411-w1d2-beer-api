af45227fde5427f466fb3811afd45c9f0722d813
jlmeister <iamphil4483@yahoo.com>
2020-01-12 03:31:22 -0600

    created simple app to fetch data about beers and display the list to the page using react. I also felt like having some fun with CSS :) ... final commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
+import beerImage from './beer.svg';
 import './App.css';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={beerImage} alt="beer" />
+        <h1>beerStuff</h1>
       </header>
     </div>
-  );
+  )
+}
+
+const BeersList = ({ list }) => {
+  return (
+    <div className="list">
+      {list.map(beer => <BeerCard key={beer.id} {...beer} />)}
+    </div>
+  )
+}
+
+class BeerCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLiked: false
+    }
+  }
+  toggleLike = () => {
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <details>
+          <summary>{this.props.name} -- {this.props.tagline} </summary>
+          <p>{this.props.abv}% ABV</p>
+          <p>
+            {this.props.description}
+          </p>
+          <p>
+            Pairs well with: {this.props.pairsWith}
+          </p>
+        </details>
+        <button onClick={this.toggleLike} className={this.state.isLiked ? "liked" : ""}>{this.state.isLiked ? "Unlike" : "Like"}</button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    beers: [],
+    isLoading: true
+  }
+
+  componentDidMount() {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(response => response.json())
+      .then(parsedJSON => parsedJSON.map(beer => (
+        {
+          id: beer.id,
+          name: beer.name,
+          tagline: beer.tagline,
+          description: beer.description,
+          abv: beer.abv,
+          pairsWith: beer.food_pairing[0]
+        }
+      )))
+      .then(beers => {
+        this.setState({
+          beers: beers,
+          isLoading: false
+        })
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <BeersList list={this.state.beers} />
+      </div>
+    )
+  }
 }
 
 export default App;
